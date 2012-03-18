56efd5243bf04594142c5ceee9c5508c06273dbb
jipiboily <jipiboily@gmail.com>
2012-03-18 16:41:16 -0400

    1.8 style hash for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Refinery::Core::Engine.routes.append do
   namespace :admin, :path => 'refinery' do
-    post "images/ajax_create", to: "images#ajax_create"
+    post "images/ajax_create", :to => "images#ajax_create"
   end
 end
