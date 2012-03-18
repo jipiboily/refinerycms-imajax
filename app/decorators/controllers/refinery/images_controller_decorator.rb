bb625d99236cb0788220dc339517cc3141e2bde0
jipiboily <jipiboily@gmail.com>
2012-03-18 16:33:46 -0400

    1.8 compatible hash

--- app/decorators/controllers/refinery/images_controller_decorator.rb
+++ app/decorators/controllers/refinery/images_controller_decorator.rb
@@ -1,14 +1,14 @@
 ::Refinery::Admin::ImagesController.class_eval do
 
   def ajax_create
-    params[:image] = images = {image: params[:image]}
+    params[:image] = {:image => params[:image]}
     params[:image].each do  |image|
       @image = ::Refinery::Image.create(params[:image])
     end
     unless @image.errors.any?
-      render :json => { redirect_to: refinery.admin_images_path }
+      render :json => { :redirect_to => refinery.admin_images_path }
     else
-      render :json => @image.errors, status: 406
+      render :json => @image.errors, :status => 406
     end
   end
 
